67954291f5d58d78de4b1c044e64560e89ec0dd3
PortugalFromMars <108102257+PortugalFromMars@users.noreply.github.com>
2024-12-19 11:58:10 +0000

    Update build.gradle.kts

--- Blog-Api/build.gradle.kts
+++ Blog-Api/build.gradle.kts
@@ -1,21 +1,11 @@
-plugins {
-    kotlin("jvm") version "2.0.21"
-}
-
-group = "org.example"
-version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
-
 dependencies {
-    testImplementation(kotlin("test"))
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(21)
+    implementation("io.ktor:ktor-server-core:2.3.5")
+    implementation("io.ktor:ktor-server-netty:2.3.5")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.5")
+    implementation("io.ktor:ktor-server-content-negotiation:2.3.5")
+    implementation("ch.qos.logback:logback-classic:1.2.11") 
+    implementation("io.ktor:ktor-server-auth:2.3.5")
+    implementation("io.ktor:ktor-server-auth-jwt:2.3.5")
+    testImplementation("io.ktor:ktor-server-tests:2.3.5")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.9.20")
 }
